1833159ba33c35ab45236f51a7cf5711523ed8a2
Lisa Herzberg <lisamarieherzberg@gmail.com>
2018-02-13 08:03:28 -0700

    two

--- toyProblems.js
+++ toyProblems.js
@@ -1,46 +1,59 @@
+
+
 //JS Basics
 //Run npm test in the command line to test your solutions
 module.exports = {
-  // reverseIt:
+  reverseIt: function (str){
+    return str.split('').reverse().join('')
+  },
 
   //uncomment and finish the reverseIt function. It will take in one parameter which is a String and
   //reverse it
 
-  // removeDups:
+  removeDups: function (array){
+    return array.filter(function(elem, pos,arr) {
+      return arr.indexOf(elem) == pos;
+  })
+},
 
-  //uncomment and finish the removeDups function. It will take in one parameter which is an Array
-  //remove all duplicates
 
-  // titleIt:
+//uncomment and finish the removeDups function. It will take in one parameter which is an Array
+//remove all duplicates
 
-  //uncomment and finish the titleIt function. It will take in one parameter which is a String and
-  //capitalize the first letter of each word
+titleIt: function (str){
+  
+}
 
-  // vowelCounter:
+//uncomment and finish the titleIt function. It will take in one parameter which is a String and
+//capitalize the first letter of each word
 
-  //uncomment and finish the vowelCounter function. It will take in one parameter which is a String and
-  //return the number of vowels in the string
+// vowelCounter:
 
-  // isPrime:
+//uncomment and finish the vowelCounter function. It will take in one parameter which is a String and
+//return the number of vowels in the string
 
-  //uncomment and finish the isPrime function. It will take in one parameter which is a Number and
-  //return true if it is prime and false if it is not
+// isPrime:
 
-  //what is the value of foo?
-  //var foo = 10 + '20';
-  //uncomment the foo property and give your answer as its value
+//uncomment and finish the isPrime function. It will take in one parameter which is a Number and
+//return true if it is prime and false if it is not
 
-  //foo:
+//what is the value of foo?
+//var foo = 10 + '20';
+//uncomment the foo property and give your answer as its value
 
-  //what is the outcome of the two console.logs below?
-  //   var foo = "Hello";
-  // (function() {
+//foo:
+
+//what is the outcome of the two console.logs below?
+//   var foo = "Hello";
+// (function() {
   //   var bar = " World";
   //   console.log(foo + bar);
   // })();
   // console.log(foo + bar);
   // uncomment the log1 and log2 properties and give your answers as their values
-
+  
   // log1:
   // log2:
+  
+  
 }
